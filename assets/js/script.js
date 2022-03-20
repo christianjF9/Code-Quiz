67be3b585289a989e498a93420e6d96c5f5090b4
Christian Foote <christianfoote999@gmail.com>
2022-03-20 02:17:20 -0400

    added css

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,7 +10,7 @@ var Question = class Question
 
 var questions = 
 [
-   new Question("why","a","wrong answer1","wrong answer2","wrong answer3"),
+   new Question("Comonly used data types DO Not Include","alerts","strings","boolean","numbers"),
    new Question("how","b","wrong answera","wrong answerb","wrong answerc"),
    new Question("when","b","wrong answera","wrong answerb","wrong answerc"),
    new Question("where","b","wrong answera","wrong answerb","wrong answerc")
@@ -19,7 +19,8 @@ var questions =
 var mainEl = document.querySelector("#main");
 var startEl = document.querySelector("#start");
 var questionEl = document.querySelector("#question");
-var feedbackEl;
+var feedbackEl = document.querySelector("#feedback");
+var infoEl = document.querySelector("#info")
 var choiceButtons = [];
 var formEl;
 
@@ -27,7 +28,7 @@ var questionIndex= 0;
 var currentQuestion;
 var time = 75;
 
-
+//picks a random question and displays it
 var NextQuestion = function()
 {
     currentQuestion = PullRandom(questions);
@@ -39,27 +40,33 @@ var NextQuestion = function()
         choiceButtons.forEach(choice => {
             choice.textContent = PullRandom(currentQuestion.choices);       
         });
+        
     }
-
 }
+
+//start the quiz
 var StartButtonHandler = function()
 {
     startEl.remove();
     for(i=0;i<4;i++)
     {
         choiceButtons.push(createEl("button","answer"));
-    }
-    
-    feedbackEl = createEl("h2","feedback");
+    }    
     Timer();
+    questionEl.className = "";
+    infoEl.className = "";
+    infoEl.textContent = "";
     NextQuestion();
     mainEl.addEventListener("click",AnswerButtonHandler);    
 }
 
+//listens for the answer button and determins if the answer is right
 var AnswerButtonHandler = function(event)
 {
     var buttonEl = event.target;
     if(buttonEl.matches(".answer"))
+    {
+        feedbackEl.className= "";
         if(buttonEl.textContent== currentQuestion.correctAnswer)
         {
             feedbackEl.textContent= "Correct!";
@@ -68,9 +75,17 @@ var AnswerButtonHandler = function(event)
         else
         {
             feedbackEl.textContent="wrong";
+            time -= 20;
         }
+        var feedbackTimeInterval = setInterval(function() {
+            feedbackEl.className = "hide"
+            clearInterval(feedbackTimeInterval);
+        },1000);
+    }
+
 }
 
+//gives initials and score to the highscore board and loads high score board 
 var SubmitHsHandler = function(event)
 {
     event.preventDefault();
@@ -83,36 +98,38 @@ var SubmitHsHandler = function(event)
         formEl.remove();
         DisplayHighScores(initials);
     }
-
 }
 
+//creates a element adds a class and adds it to main
 var createEl = function(type, styleClass){
     var el = document.createElement(type);
     el.className = styleClass;
-    mainEl.appendChild(el);
+    feedbackEl.before(el)
     return el;
 }
 
+// returns a random element of an array and removes it from the array
 var PullRandom = function(array)
 {
     var index = Math.floor(Math.random()* array.length-1)
     return array.splice(index,1)[0];
 }
 
+// tells the user their score and ask them to fill out their initials
 var showScore = function()
 {
-    console.log("showing score");
     choiceButtons.forEach(buttonEl =>{
         buttonEl.remove();
     });
     questionEl.textContent = "All done";
-    yourScoreEl = createEl("div","yourScore");
-    yourScoreEl.textContent = "your final score is: " +time;
+    infoEl.textContent ="";
+    infoEl.textContent = "your final score is: " +time;
     formEl = createEl("form","");
-    formEl.innerHTML ='<input type="text" name="initials" placeholder="Enter initials" /><button class="" type="submit">submit</button> ';
+    formEl.innerHTML ='<input type="text" name="initials" placeholder="Enter initials" /><button class="small-button" type="submit">submit</button> ';
     formEl.addEventListener("submit",SubmitHsHandler)
 }
 
+//loads the highscores and put the users scores in right part of the highscore board
 var DisplayHighScores = function(initials)
 {
     questionEl.textContent ="";
@@ -120,11 +137,9 @@ var DisplayHighScores = function(initials)
     var CurrentEntry = initials +"- "+time;
     var myScore = parseInt(CurrentEntry.slice(CurrentEntry.length-2,CurrentEntry.length));
     var index =0;
-    //highScores.push(myentry);
     highScores = loadHighScores();
     console.log(highScores);
     var score = highScores[index];
-    //score = parseInt(score.slice(score.length-2));
     console.log(myScore);
     index =0;
 
@@ -140,28 +155,28 @@ var DisplayHighScores = function(initials)
         console.log(index);
         console.log(score);
     }
-  //  while(myScore<=score&& index < highScores.length) {
-
-   // }   
     hs2 = highScores.slice(index);
     console.log(hs2);
     highScores.splice(index);
     console.log(highScores);
     highScores.push(CurrentEntry);
     highScores = highScores.concat(hs2);
-
-   // highScores.copyWithin(index,highScores.length-1,highScores.length);
-   // highScores.pop();
     console.log(highScores);
     highScores.forEach(score=>{
        var scoreEl = createEl("div","high-score");
        scoreEl.textContent = score;
     });
-    saveEl = createEl("button","");
+    saveEl = createEl("button","small-button");
     saveEl.textContent = "save";
     saveEl.addEventListener("click",SaveHighScores(highScores));
-    mainEl.innerHTML += '<input type="button" value="go back" onClick="window.location.reload(true)"></input>'
+    backEl = createEl("button","small-button");
+    backEl.textContent = "go back";
+    backEl.addEventListener("click",BackHandler);
 }
+
+    var BackHandler = function(){
+        location.reload();
+    }
 
 var SaveHighScores = function(highScores){
     localStorage.setItem("highscore", JSON.stringify(highScores));
@@ -173,6 +188,7 @@ var loadHighScores = function(){
     if(!highScores) return [];
     else return highScores;
 }
+
 var Timer = function() {
     var timerEl = document.querySelector("#timer");
     var timeInterval = setInterval(function() {
@@ -184,8 +200,6 @@ var Timer = function() {
       else{
         time--;
         timerEl.textContent = "time: "+time; 
-
-  
       }
     },1000);
 }
